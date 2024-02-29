4e63fdb226fc34186553ff9dc25f7c17e39157c7
Edgar SA <Garko1209@hotmail.com>
2024-02-29 12:50:09 -0600

    se modifico el holamundo

--- holamundo.java
+++ holamundo.java
@@ -2,6 +2,6 @@ public class holamundo{
     public static void main(String [] args){
         System.out.println("Hola mundo");
         System.out.println("Estamos practicando git");
-        System.out.println("esta es una nueva moficacion");
+        
     }
 }
