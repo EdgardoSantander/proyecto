442bdf3552d73738bf9e9eac225282af8a948366
Edgar SA <Garko1209@hotmail.com>
2024-03-01 20:39:37 -0600

    Se agrego la clase Frutas

---
+++ visual/Frutas.java
@@ -0,0 +1,11 @@
+package visual;
+
+public class Frutas {
+    private int cantidad;
+    private String nombre;
+
+
+    public void cuantasFrutas(int cuantas){
+        this.cantidad = cuantas;
+    }
+}
