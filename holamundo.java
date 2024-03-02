20a16c8b76327bd6154a0de50c4c205bfc3259ba
Edgar SA <Garko1209@hotmail.com>
2024-03-01 20:44:06 -0600

    Agregamos los cambios en la rama main

--- holamundo.java
+++ holamundo.java
@@ -2,6 +2,6 @@ public class holamundo{
     public static void main(String [] args){
         System.out.println("Hola mundo");
         System.out.println("Estamos practicando git");
-        
+        System.out.println(" Ahora volvimos a la rama main pero los cambios hechos en la rama compo no existen aqui");
     }
 }
